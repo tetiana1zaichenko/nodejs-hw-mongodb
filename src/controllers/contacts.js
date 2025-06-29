6fe07248b7331316d7d27199f060417f8d2c79be
Tetiana_Zaichenko <tanja.zaychenko@gmail.com>
2025-06-29 14:07:30 +0300

    add task 4 sort

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -7,13 +7,17 @@ import {
 } from '../services/contacts.js';
 
 import createHttpError from 'http-errors';
+import { parseSortParams } from '../utils/parseSortParams.js';
 
 export async function getContactsController(req, res) {
   const page = parseInt(req.query.page) || 1;
   const perPage = parseInt(req.query.perPage || 10);
+  const { sortBy, sortOrder } = parseSortParams(req.query);
   const contacts = await getAllContactsService({
     page,
     perPage,
+    sortBy,
+    sortOrder,
   });
   res.status(200).json({
     status: 200,
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,7 +1,13 @@
 import { ContactsModel } from '../models/contactModel.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
+import { SORT_ORDER } from '../constants/index.js';
 
-export async function getAllContactsService({ page, perPage }) {
+export async function getAllContactsService({
+  page,
+  perPage,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = 'name',
+}) {
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
@@ -10,7 +16,11 @@ export async function getAllContactsService({ page, perPage }) {
     .merge(contactsQuery)
     .countDocuments();
 
-  const contacts = await contactsQuery.skip(skip).limit(limit).exec();
+  const contacts = await contactsQuery
+    .skip(skip)
+    .limit(limit)
+    .sort({ [sortBy]: sortOrder })
+    .exec();
 
   const paginationData = calculatePaginationData(contactsCount, perPage, page);
 

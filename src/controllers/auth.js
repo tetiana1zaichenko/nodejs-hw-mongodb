aefdf742bcc34e44d4c07b2d366d188994914ba3
Tetiana_Zaichenko <tanja.zaychenko@gmail.com>
2025-07-05 23:15:51 +0300

    refreshUsersSession

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -4,6 +4,7 @@ import { ONE_DAY } from '../constants/index.js';
 import { refreshSession } from '../services/auth.js';
 import createHttpError from 'http-errors';
 import { logoutUser } from '../services/auth.js';
+import { refreshUsersSession } from '../services/auth.js';
 
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
@@ -75,4 +76,32 @@ export const logoutUserController = async (req, res) => {
   res.clearCookie('refreshToken');
 
   res.status(204).send();
+};
+
+const setupSession = (res, session) => {
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+};
+
+export const refreshUserSessionController = async (req, res) => {
+  const session = await refreshUsersSession({
+    sessionId: req.cookies.sessionId,
+    refreshToken: req.cookies.refreshToken,
+  });
+
+  setupSession(res, session);
+
+  res.json({
+    status: 200,
+    message: 'Successfully refreshed a session!',
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
 };
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -9,6 +9,7 @@ import { validateBody } from '../middlewares/validateBody.js';
 import { loginUserSchema } from '../validation/auth.js';
 import { loginUserController } from '../controllers/auth.js';
 import { logoutUserController } from '../controllers/auth.js';
+import { refreshUserSessionController } from '../controllers/auth.js';
 
 const router = Router();
 
@@ -24,8 +25,10 @@ router.post(
   ctrlWrapper(loginUserController),
 );
 
-router.post('/refresh', ctrlWrapper(refreshSessionController));
+// router.post('/refresh', ctrlWrapper(refreshSessionController));
 
 router.post('/logout', ctrlWrapper(logoutUserController));
+
+router.post('/refresh', ctrlWrapper(refreshUserSessionController));
 
 export default router;

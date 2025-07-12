cae5088a3bc6a293dd3457480cd0e20e78020a1d
Tetiana_Zaichenko <tanja.zaychenko@gmail.com>
2025-07-12 22:51:25 +0300

    reset password work

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -6,6 +6,7 @@ import createHttpError from 'http-errors';
 import { logoutUser } from '../services/auth.js';
 import { refreshUsersSession } from '../services/auth.js';
 import { requestResetToken } from '../services/auth.js';
+import { resetPassword } from '../services/auth.js';
 
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
@@ -111,6 +112,15 @@ export const requestResetEmailController = async (req, res) => {
   await requestResetToken(req.body.email);
   res.json({
     message: 'Reset password email was successfully sent!',
+    status: 200,
+    data: {},
+  });
+};
+
+export const resetPasswordController = async (req, res) => {
+  await resetPassword(req.body);
+  res.json({
+    message: 'Password was successfully reset!',
     status: 200,
     data: {},
   });

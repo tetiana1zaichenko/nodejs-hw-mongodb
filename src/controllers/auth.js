dc14f6d6d4c2028eb56945c1188808df5472f443
Tetiana_Zaichenko <tanja.zaychenko@gmail.com>
2025-07-14 10:40:18 +0300

    fixed rename rout /auth/send-reset-email and /auth/reset-pwd

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -36,7 +36,6 @@ export const loginUserController = async (req, res) => {
     message: 'Successfully logged in an user!',
     data: {
       accessToken: session.accessToken,
-      refreshToken: session.refreshToken,
     },
   });
 };

807ffecc29aaed6b4d5297528f2783eb87fc3a56
Tetiana_Zaichenko <tanja.zaychenko@gmail.com>
2025-07-17 09:56:06 +0300

    add task 7 роут /api-docs

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 export function setupServer() {
   const app = express();
@@ -16,6 +17,9 @@ export function setupServer() {
   app.use(pino());
   app.use(express.json());
   app.use(cookieParser());
+
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use('/contacts', contactsRouter);
   app.use('/auth', authRouter);
